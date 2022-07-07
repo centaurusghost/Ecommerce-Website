d9af36e24fcf9e72b73c7479cf5c94c02e56d83a
Ozan Wagle <ozonewagle998@gmail.com>
2022-07-07 09:07:00 +0545

    New O&B logo

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
     <!-- first child -->
     <nav class="navbar navbar-expand-lg bg-warning">
   <div class="container-fluid">
-    <img src="./images/logo.png" alt="" class="MainLogo">
+    <img src="./images/mainlogo.png" alt="" class="MainLogo">
     <a class="navbar-brand" href="#">Logo</a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
